29ffd5248b7b19ba44394a83e211971a32c407d9
TinaLien <tinalien0728@gmail.com>
2024-01-31 10:56:23 +0800

    Homework 2 register for check user name,2024/01/31

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -35,5 +35,13 @@ namespace MSIT155Site.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+        public IActionResult City()
+        {
+            return View();
+        }
+        public IActionResult Avator()
+        {
+            return View();
+        }
     }
 }
